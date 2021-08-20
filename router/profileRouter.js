cd7f1bb2cdd3be4d102e7c4ddea963b7e1c18b64
erenonur19 <eok@eskisehir.edu.tr>
2021-08-20 17:26:02 +0300

    profile-cont

--- router/profileRouter.js
+++ router/profileRouter.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router3=express.Router();
 const User=require('../models/userModel')
 const verify=require('./verifyToken')
+const Post=require('../models/postModel')
 
 
 
@@ -9,6 +10,22 @@ router3.get('/',verify,async(req,res,next)=>{
     try{
         const user1=await User.findById({_id:req.user._id})
     res.json(user1)}
+    catch(err){
+        console.log(err);
+    }
+
+
+
+})
+
+router3.get('/posts',verify,async(req,res,next)=>{
+    try{
+       let user=await User.findById({_id:req.user._id})
+       let username=user.userName;
+       let posts=await Post.find({userName:username})
+       res.json(posts)
+      
+    }
     catch(err){
         console.log(err);
     }
